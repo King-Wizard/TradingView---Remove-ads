4a1449b218be63ad855b716e36ba458352c5b8ce
King-Wizard <king.wizard.contact@gmail.com>
2024-12-26 21:33:48 +0100

    First commit

---
+++ TradingViewNoAds_infinite_loop_version.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         TradingView - Remove ads - infiniteLoopVersion
+// @version      1.0
+// @description  Does what it says.
+// @author       King Wizard
+// @icon         https://i.ibb.co/3pdHtQg/trading-view-icon.png
+// @include      https://www.tradingview.com/chart/*
+// @grant        none
+// @namespace    KingWizardNamespace
+// ==/UserScript==
+
+// https://play-lh.googleusercontent.com/OoPpCoMOpPra6k0Qlwhfi14vOmqHfR790C1C7a_sHNqqUUYJhTYy6rjAmrv4eMLQRDbJ=s94-rw
+
+window.addEventListener('load', function infiniteLoopVersion() {
+    'use strict'
+
+    //console.log('%cPage fully loaded', 'background: purple; color: black; font-weight: bold')
+
+    //let logCounter = 0
+
+    setInterval(function() {
+        //console.log('%clogCounter = ' + ++logCounter, 'background: white; color: black; font-weight: bold');
+
+        const adBox = document.querySelector('iframe[title="3rd party ad content"]')
+        if(adBox){
+            adBox.remove()
+            //console.log('%cadBox removed', 'background: blue; color: black; font-weight: bold')
+            //logCounter = 0
+        }/*else{
+            console.log('%cNo adBox for the moment', 'background: yellow; color: black; font-weight: bold')
+        }*/
+    }, 1000)
+
+})
