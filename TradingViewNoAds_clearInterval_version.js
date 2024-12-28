a04d38fd53610ede4950a1bdddd5b6168e3917df
King-Wizard <king.wizard.contact@gmail.com>
2024-12-29 00:44:25 +0100

    Update TradingViewNoAds_clearInterval_version.js

--- TradingViewNoAds_clearInterval_version.js
+++ TradingViewNoAds_clearInterval_version.js
@@ -26,21 +26,21 @@ window.addEventListener('load', function clearIntervalVersion() {
                                    `font-weight: bold`)
     }
 
-    consolePrintLine('Web page fully loaded', 'black', 'purple')
+    consolePrintLine(`Web page fully loaded`, `black`, `purple`)
 
     let logCounter = 0
     */
 
     let intervalID = setInterval(function() {
-        //consolePrintLine(`logCounter = ${++logCounter}`, 'black', 'white')
+        //consolePrintLine(`logCounter = ${++logCounter}`, `black`, `white`)
 
         const adBox = document.querySelector('iframe[title="3rd party ad content"]')
         if(adBox){
             adBox.remove()
-            //consolePrintLine('adBox removed', 'black', 'blue')
+            //consolePrintLine(`adBox removed`, `black`, `blue`)
             clearInterval(intervalID)
         }/*else{
-            consolePrintLine('No adBox for the moment', 'black', 'yellow')
+            consolePrintLine(`No adBox for the moment`, `black`, `yellow`)
         }*/
     }, 1000)
 
